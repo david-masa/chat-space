5db64534102086e36081cf4f582bf1a0f96c86ce
david-masa <apple.phone.3g.masa@gmail.com>
2020-05-11 17:47:17 +0900

    テスト

--- db/migrate/20200328110220_create_messages.rb
+++ db/migrate/20200328110220_create_messages.rb
@@ -2,6 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :content
+      t.string :youtube_url
       t.string :image
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
